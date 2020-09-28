a589d33e24cf91095f67dd8e14cfe9cd85ad382e
brunopaa <b.passamai@gmail.com>
2020-09-28 01:28:45 -0300

    update roteiro aula 22

--- Atividades/exercicios/laravel/sistema/routes/web.php
+++ Atividades/exercicios/laravel/sistema/routes/web.php
@@ -12,7 +12,45 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Models\Estado;
+use App\Models\Produto;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/ola', function () {
+    return 'Olá,mundo!';
+});
+
+Route::get('/estados/todos', function () {
+    $estados = Estado::all();
+    return view('lista', ['dados' => $estados]);
+});
+
+Route::get('/produtos/todos', function () {
+    $produtos = Produto::all();
+    return view('lista', ['dados' => $produtos]);
+});
+
+Route::get('/estados/{id}', function ($id) {
+
+    $estado = Estado::findOrFail($id);
+
+    if ($estado == null) {
+        return 'ID Inválido';
+    }
+
+    return view('lista',['dados'=>$estado]);
+});
+
+Route::get('/produtos/{id}', function ($id) {
+
+    $produto = Produto::findOrFail($id);
+
+    if ($produto == null) {
+        return 'ID Inválido';
+    }
+
+    return view('lista',['dados'=>$produto]);
 });

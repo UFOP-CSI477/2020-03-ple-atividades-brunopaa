495fef9d5ec36a424fc0f1da3579a85872f7e014
brunopaa <b.passamai@gmail.com>
2020-09-28 00:48:29 -0300

    update roteiro aula 21

---
+++ Atividades/exercicios/laravel/sistema/database/factories/ProdutoFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Produto;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProdutoFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Produto::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'nome' => $this->faker->word,
+            'um' => $this->faker->randomElement($array = array('mm','cm','pc', 'kg', 'l', 'm'))
+
+        ];
+    }
+}
